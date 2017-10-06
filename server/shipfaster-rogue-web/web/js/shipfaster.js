42430c94aa6e5d1bd43674bc5865c498f939f09c
Simon Rigg <simon.rigg@criticalblue.com>
2017-10-06 17:02:14 +0100

    jquery test

---
+++ server/shipfaster-rogue-web/web/js/shipfaster.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+    $("body").css({"background-image":"url('images/fire.jpg')"});
+})
+
+$("#send-bitcoin-button").click(function() {
+    alert("Pay me the money!")
+})
+
+$("#search-shipments-button").click(function() {
+    alert("Searching shipments...")
+})

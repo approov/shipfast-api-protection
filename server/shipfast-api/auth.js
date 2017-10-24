f9a91f6deec5198614e17d64a819d3df51d13550
Simon Rigg <simon.rigg@criticalblue.com>
2017-10-24 15:01:25 +0100

    allow demo to be configured

--- server/shipfast-api/auth.js
+++ server/shipfast-api/auth.js
@@ -12,6 +12,7 @@ const router = express.Router()
 const jwt = require('express-jwt')
 const jwksRsa = require('jwks-rsa')
 const crypto = require('crypto')
+const config = require('./demo-configuration')
 
 // The array of ShipFast API keys
 var shipFastAPIKeys = [
@@ -43,33 +44,52 @@ router.use(function(req, res, next) {
     return
   }
 
-  // Retrieve the ShipFast HMAC used to sign the API request from the request header
-  var requestShipFastHMAC = req.get('SF-HMAC')
-  if (!requestShipFastHMAC) {
-    console.log('\tShipFast HMAC not specified or in the wrong format')
-    res.status(400).send()
-    return
-  }
+  // Configure the request HMAC verification based on the demo stage
+  if (config.DEMO_STAGE == DEMO_STAGE.HMAC_STATIC_SECRET_PROTECTION
+      || config.DEMO_STAGE == DEMO_STAGE.HMAC_DYNAMIC_SECRET_PROTECTION) {
 
-  // Calculate our version of the HMAC and compare with one sent in the request header
-  var secret = SHIPFAST_HMAC_SECRET
-  var obfuscatedSecretData = Buffer.from(secret, 'base64')
-  var shipFastAPIKeyData = new Buffer("QXBwcm9vdidzIHRvdGFsbHkgYXdlc29tZSEh")
-  for (var i = 0; i < Math.min(obfuscatedSecretData.length, shipFastAPIKeyData.length); i++) {
-    obfuscatedSecretData[i] ^= shipFastAPIKeyData[i]
-  }
-  var obfuscatedSecret = new Buffer(obfuscatedSecretData).toString('base64')
-  var hmac = crypto.createHmac('sha256', Buffer.from(/*secret*/obfuscatedSecret, 'base64'))
-  hmac.update(req.protocol)
-  hmac.update(req.host)
-  hmac.update(req.originalUrl)
-  hmac.update(req.get('Authorization'))
-  var ourShipFastHMAC = hmac.digest('hex')
-  if (ourShipFastHMAC != requestShipFastHMAC) {
-    console.log("\tShipFast HMAC invalid: received " + requestShipFastHMAC
-      + " but should be " + ourShipFastHMAC)
-    res.status(403).send()
-    return
+    // Retrieve the ShipFast HMAC used to sign the API request from the request header
+    var requestShipFastHMAC = req.get('SF-HMAC')
+    if (!requestShipFastHMAC) {
+      console.log('\tShipFast HMAC not specified or in the wrong format')
+      res.status(400).send()
+      return
+    }
+
+    // Calculate our version of the HMAC and compare with one sent in the request header
+    var secret = SHIPFAST_HMAC_SECRET
+    var hmac
+    if (config.DEMO_STAGE == DEMO_STAGE.HMAC_STATIC_SECRET_PROTECTION) {
+      // Just use the static secret during HMAC verification for this demo stage
+      hmac = crypto.createHmac('sha256', Buffer.from(secret, 'base64'))
+    }
+    else if (config.DEMO_STAGE == DEMO_STAGE.HMAC_DYNAMIC_SECRET_PROTECTION) {
+      // Obfuscate the static secret to produce a dymanic secret to use during HMAC
+      // verification for this demo stage
+      var obfuscatedSecretData = Buffer.from(secret, 'base64')
+      var shipFastAPIKeyData = new Buffer("QXBwcm9vdidzIHRvdGFsbHkgYXdlc29tZSEh")
+      for (var i = 0; i < Math.min(obfuscatedSecretData.length, shipFastAPIKeyData.length); i++) {
+        obfuscatedSecretData[i] ^= shipFastAPIKeyData[i]
+      }
+      var obfuscatedSecret = new Buffer(obfuscatedSecretData).toString('base64')
+      hmac = crypto.createHmac('sha256', Buffer.from(obfuscatedSecret, 'base64'))
+    }
+    
+    // Compute the request HMAC using the HMAC SHA-256 algorithm
+    hmac.update(req.protocol)
+    hmac.update(req.host)
+    hmac.update(req.originalUrl)
+    hmac.update(req.get('Authorization'))
+    var ourShipFastHMAC = hmac.digest('hex')
+
+    // Check to see if our HMAC matches the one sent in the request header
+    // and send an error response if it doesn't
+    if (ourShipFastHMAC != requestShipFastHMAC) {
+      console.log("\tShipFast HMAC invalid: received " + requestShipFastHMAC
+        + " but should be " + ourShipFastHMAC)
+      res.status(403).send()
+      return
+    }
   }
 
   next()
---
+++ server/shipfast-api/demo-configuration.js
@@ -0,0 +1,30 @@
+/*****************************************************************************
+ * Project:     ShipFast API Protection (Server)
+ * File:        demo-configuration.js
+ * Original:    Created on 24 Oct 2017 by Simon Rigg
+ * Copyright(c) 2002 - 2017 by CriticalBlue Ltd.
+ * 
+ * A file for configuring the ShipFast demo.
+ *****************************************************************************/
+
+// The enumeration of various stages of the demo.
+const DEMO_STAGE = {
+    // The demo which uses basic protection by way of API key specified in the app manifest
+    API_KEY_PROTECTION: 0,
+    // The demo which introduces API request signing by HMAC using a static secret in code
+    HMAC_STATIC_SECRET_PROTECTION: 1,
+    // The demo which introduces API request signing by HMAC using a dynamic secret in code
+    HMAC_DYNAMIC_SECRET_PROTECTION: 2,
+    // The demo which uses CriticalBlue Approov protection by authenticating the app
+    APPROOV_APP_AUTH_PROTECTION: 3
+}
+
+var config = {}
+
+// The current demo stage
+config.currentDemoStage = DEMO_STAGE.API_KEY_PROTECTION
+
+module.exports = {
+    DEMO_STAGE,
+    config 
+}

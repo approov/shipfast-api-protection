b1cebd9025c9467f7bafe167f0d4475dc882991c
Paulo Silva <paulos@criticalblue.com>
2019-10-31 13:43:58 +0000

    Fixed the docker stack to run the full stack in localhost.
    Signed-off-by: Paulo Silva <paulos@criticalblue.com>

--- docker/setup/docker-volumes-dirs.sh
+++ docker/setup/docker-volumes-dirs.sh
@@ -2,12 +2,13 @@
 
 set -eu
 
-PROJECT_HOST_DIR=${1? Missing Project host dir.}
-VERSION=${2? Missing Android studio version.}
+PROJECT_NAME=${1? Missing Project name.}
+PROJECT_HOST_DIR=${2? Missing Project host dir.}
+VERSION=${3? Missing Android studio version.}
 
 mkdir -v -p \
-    ~/.docker-shipfast-demo/android-studio/${VERSION}/${PROJECT_HOST_DIR}/Android \
-    ~/.docker-shipfast-demo/android-studio/${VERSION}/${PROJECT_HOST_DIR}/.AndroidStudio${VERSION} \
-    ~/.docker-shipfast-demo/android-studio/${VERSION}/${PROJECT_HOST_DIR}/.android \
-    ~/.docker-shipfast-demo/android-studio/${VERSION}/${PROJECT_HOST_DIR}/.gradle \
-    ~/.docker-shipfast-demo/android-studio/${VERSION}/${PROJECT_HOST_DIR}/.java \
+    "${PROJECT_HOST_DIR}"/android-studio/${VERSION}/"${PROJECT_NAME}"/Android \
+    "${PROJECT_HOST_DIR}"/android-studio/${VERSION}/"${PROJECT_NAME}"/.AndroidStudio${VERSION} \
+    "${PROJECT_HOST_DIR}"/android-studio/${VERSION}/"${PROJECT_NAME}"/.android \
+    "${PROJECT_HOST_DIR}"/android-studio/${VERSION}/"${PROJECT_NAME}"/.gradle \
+    "${PROJECT_HOST_DIR}"/android-studio/${VERSION}/"${PROJECT_NAME}"/.java \
